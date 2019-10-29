5aacd2134cfe28d0180e5c9b1194c183a6c7f33e
5ymph0en1x <35420422+5ymph0en1x@users.noreply.github.com>
2019-10-29 09:17:39 +0100

    Update formater_piphist.py

--- EXAMPLE/Symphoenix/formater_piphist.py
+++ EXAMPLE/Symphoenix/formater_piphist.py
@@ -1,38 +1,73 @@
 import csv
 from datetime import datetime
+import pandas as pd
 from histdata import download_hist_data as dl
 from histdata.api import Platform as P, TimeFrame as TF
 import zipfile
 
+###############################
 symbol = "USDCAD"
-months = 10
-
-# csv file name
+from_year = 2018
+until_year = 2019
+until_month = 10
+###############################
 
 rows = []
+current_year = int(datetime.now().year)
+current_month = int(datetime.now().month)
 
-for i in range(1, months):
-    zipfiles = dl(year='2019', month=i, pair=symbol, platform=P.GENERIC_ASCII, time_frame=TF.ONE_MINUTE)
-    filename = "DAT_ASCII_%s_M1_20190%s" % (symbol, str(i))
-    zip_filename = "./" + filename + ".zip"
-    csv_filename = filename + ".csv"
-    with zipfile.ZipFile(zip_filename) as z:
-        z.extract(csv_filename)
-        with open(csv_filename, 'r') as csvfile:
-            csvreader = csv.reader(csvfile, delimiter=';')
-            # extracting each data row one by one
-            for row in csvreader:
-                # print(row[0])
-                row_time = datetime.strptime(row[0], '%Y%m%d %H%M%S')
-                row_ = row_time, row[4]
-                rows.append(row_)
+if until_year > current_year:
+    until_year = current_year
 
-with open('STATICS/FX/2019/%s.csv' % symbol, mode='w', newline='') as ticker:
-    ticker_writer = csv.writer(ticker, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    headers = 'date', 'close'
-    ticker_writer.writerow(headers)
-    for row in rows:
-        ticker_writer.writerow(row)
+if from_year > current_year:
+    from_year = current_year
+
+if until_month > current_month or until_month > 12:
+    until_month = current_month
+
+for j in range(from_year, until_year + 1):
+    if j != current_year:
+        zipfiles = dl(year=j, month=None, pair=symbol, platform=P.GENERIC_ASCII, time_frame=TF.ONE_MINUTE)
+        filename = "DAT_ASCII_%s_M1_%s" % (symbol, str(j))
+        zip_filename = "./" + filename + ".zip"
+        csv_filename = filename + ".csv"
+        with zipfile.ZipFile(zip_filename) as z:
+            z.extract(csv_filename)
+            with open(csv_filename, 'r') as csvfile:
+                csvreader = csv.reader(csvfile, delimiter=';')
+                # extracting each data row one by one
+                for row in csvreader:
+                    # print(row[0])
+                    row_time = datetime.strptime(row[0], '%Y%m%d %H%M%S')
+                    row_ = row_time, row[4]
+                    rows.append(row_)
+        continue
+    for i in range(1, until_month + 1):
+        zipfiles = dl(year=j, month=i, pair=symbol, platform=P.GENERIC_ASCII, time_frame=TF.ONE_MINUTE)
+        if i < 10:
+            filename = "DAT_ASCII_%s_M1_%s0%s" % (symbol, str(j), str(i))
+        if i >= 10:
+            filename = "DAT_ASCII_%s_M1_%s%s" % (symbol, str(j), str(i))
+        zip_filename = "./" + filename + ".zip"
+        csv_filename = filename + ".csv"
+        with zipfile.ZipFile(zip_filename) as z:
+            z.extract(csv_filename)
+            with open(csv_filename, 'r') as csvfile:
+                csvreader = csv.reader(csvfile, delimiter=';')
+                # extracting each data row one by one
+                for row in csvreader:
+                    # print(row[0])
+                    row_time = datetime.strptime(row[0], '%Y%m%d %H%M%S')
+                    row_ = row_time, row[4]
+                    rows.append(row_)
+
+time_start = rows[0][0]
+time_end = rows[len(rows) - 1][0]
+index = pd.date_range(time_start, time_end, freq='min')
+ts = pd.DataFrame(rows, columns=['date', 'close'])
+ts.set_index('date', drop=True, inplace=True)
+ts = ts.reindex(index=index, method='pad')
+ts = ts[ts.index.dayofweek < 5]
+ts.to_csv(path_or_buf='STATICS/FX/%s.csv' % symbol, index=True, index_label='date')
 
 csvfile.close()
-ticker.close()
